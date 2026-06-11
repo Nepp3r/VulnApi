262eaa42c11a2f11fdf062c3a5cd0af1bb21ddb2
Nepp3r <plstas06@gmail.com>
2026-06-11 13:59:57 +0200

    Add a Deleted and Author properties to Movie entity and corresponding domain entity methods

--- src/VulnApi/VulnAPI.Domain/Movie/Movie.cs
+++ src/VulnApi/VulnAPI.Domain/Movie/Movie.cs
@@ -10,7 +10,7 @@ namespace VulnAPI.Domain.Movie
 
         private Movie() { }
 
-        public static Movie Create(string title, string description, DateTime releaseDate, HashSet<Genre> genres)
+        public static Movie Create(string title, string description, string author, DateTime releaseDate, HashSet<Genre> genres)
         {
             if (string.IsNullOrWhiteSpace(title))
                 throw new ArgumentException("Title is required", nameof(title));
@@ -19,7 +19,9 @@ namespace VulnAPI.Domain.Movie
             {
                 Title = title,
                 Description = description,
-                ReleaseDate = releaseDate
+                Author = author,
+                ReleaseDate = releaseDate,
+                Deleted = false,
                 _genres = genres
             };
         }
@@ -27,7 +29,21 @@ namespace VulnAPI.Domain.Movie
         public int Id { get; private set; }
         public string Title { get; private set; }
         public string Description { get; private set; }
+        public string Author { get; private set; }
         public DateTime ReleaseDate { get; private set; }
+        public bool Deleted { get; private set; }
         public IReadOnlyCollection<Genre> Genres => _genres.AsReadOnly();
+        public void Delete()
+        {
+            if (Deleted)
+                throw new InvalidOperationException("Movie already has been deleted");
+            Deleted = true;
+        }
+        public void Restore()
+        {
+            if (!Deleted)
+                throw new InvalidOperationException("Movie is not deleted");
+            Deleted = false;
+        }
     }
 }
