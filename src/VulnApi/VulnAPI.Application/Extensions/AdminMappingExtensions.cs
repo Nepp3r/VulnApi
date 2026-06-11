01fefee89d3eb196ebcffcaadbd88ca98830a70b
Nepp3r <plstas06@gmail.com>
2026-06-11 13:48:54 +0200

    Add Admin functionality service and mapping extensions between domain entities and DTOs

---
+++ src/VulnApi/VulnAPI.Application/Extensions/AdminMappingExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using VulnAPI.Application.DTOs.Admin;
+using VulnAPI.Domain.Admin;
+
+namespace VulnAPI.Application.Extensions
+{
+    public static class AdminMappingExtensions
+    {
+        public static ReportDto ToDTO(this Report report)
+        {
+            return new ReportDto
+            {
+                AuthorId = report.Author,
+                Title = report.Title,
+                Text = report.Text,
+                CreatedAt = report.CreatedAt,
+                ClosedAt = report.ClosedAt,
+                Status = report.Status.ToString()
+            };
+        }
+    }
+}
---
+++ src/VulnApi/VulnAPI.Application/Services/AdminService.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VulnAPI.Application.DTOs.Admin;
+using VulnAPI.Application.Extensions;
+using VulnAPI.Application.Interfaces;
+using VulnAPI.Domain.Admin;
+using VulnAPI.Domain.Post;
+using VulnAPI.Domain.User;
+
+namespace VulnAPI.Application.Services
+{
+    public class AdminService
+    {
+        private readonly IVulnApiDbContext _dbContext;
+        public AdminService(IVulnApiDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task BlockUserByIdAsync(Guid userId, string? reason, TimeSpan? duration)
+        {
+            User? user = _dbContext.Users.FirstOrDefault(u => u.Id.Equals(userId));
+            if (user == null)
+                throw new ApplicationException("User with given ID was not found");
+            if (duration == null)
+                user.Block(reason, duration);
+            else
+                user.BlockPermanently(reason);
+            
+            await _dbContext.SaveChangesAsync();
+        }
+        public async Task DeletePostByIdAsync(int postId)
+        {
+            Post? post = _dbContext.Posts.FirstOrDefault(p => p.Id == postId);
+            if (post == null)
+                throw new ApplicationException("Post with given ID was not found");
+            post.Delete();
+            await _dbContext.SaveChangesAsync();
+        }
+        public async Task<List<ReportDto>> GetPendingReportsAsync()
+        {
+            var reports = _dbContext.Reports
+                .Where(r => r.Status != ReportStatus.Closed)
+                .Select(r => r.ToDTO())
+                .ToList();
+            return reports;
+        }
+        public async Task<List<ReportDto>> GetAllReportsAsync()
+        {
+            var reports = _dbContext.Reports
+                .Select(r => r.ToDTO())
+                .ToList();
+            return reports;
+        }
+    }
+}
