921644dbd71778bff2291ef5950982aea3cbdcf3
Nepp3r <plstas06@gmail.com>
2026-06-11 13:06:36 +0200

    Add an interface for application database context to implement repository pattern

---
+++ src/VulnApi/VulnAPI.Application/Interfaces/IVulnApiDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Threading;
+using System.Threading.Tasks;
+using VulnAPI.Domain.Admin;
+using VulnAPI.Domain.Movie;
+using VulnAPI.Domain.Post;
+using VulnAPI.Domain.User;
+using VulnAPI.Domain.User.Profile;
+using VulnAPI.Domain.Watchlist;
+
+namespace VulnAPI.Application.Interfaces
+{
+    public interface IVulnApiDbContext
+    {
+        DbSet<User> Users { get; }
+        DbSet<Post> Posts { get; }
+        DbSet<WatchlistItem> WatchlistItems{ get; }
+        DbSet<Movie> Movies { get; }
+        DbSet<Follow> Follows { get; }
+        DbSet<Profile> Profiles { get; }
+        DbSet<Report> Reports { get; }
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+    }
+}
